c781ce24f7ca6244e4aa8d6c5ec75700856f661b
Russell Snyder <russell@rustybass.comr>
2023-05-23 14:34:20 +0200

    add ant design system

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,52 @@
-import "./App.css";
+import { Layout, Space, Typography } from "antd";
+import "antd/dist/reset.css";
+import "./App.module.css";
+import "./index.css";
+
+import {} from "antd/es/layout/layout";
 import { IssueViewer } from "./components/IssuesViewer";
+import { green } from "@ant-design/colors";
+import {
+  GithubOutlined,
+  BarsOutlined,
+  SettingFilled,
+  SmileOutlined,
+  SyncOutlined,
+} from "@ant-design/icons";
 
 function App() {
   return (
-    <div className="App">
-      <IssueViewer />
+    <div
+      className="App"
+      style={{
+        width: "100%",
+        maxWidth: 1400,
+        margin: "auto",
+      }}
+    >
+      <Space direction="vertical" size={[0, 100]} style={{ width: "100%" }}>
+        <Layout>
+          <Layout.Header
+            style={{
+              backgroundColor: green[6],
+              textAlign: "center",
+            }}
+          >
+            <Typography.Title
+              level={2}
+              style={{ marginTop: 10, color: "white" }}
+            >
+              <GithubOutlined />
+              <span style={{ margin: "0 20px" }}>React Issue Viewer</span>
+              <BarsOutlined />
+            </Typography.Title>
+          </Layout.Header>
+          <Layout.Content>
+            <IssueViewer />
+          </Layout.Content>
+          <Layout.Footer>Footer</Layout.Footer>
+        </Layout>
+      </Space>
     </div>
   );
 }
